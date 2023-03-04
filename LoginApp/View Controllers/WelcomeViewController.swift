8df6379c525b38561cbd339e4886f061624db03d
Viktor Teslenko <meex_v92@mail.ru>
2023-03-04 12:42:04 +0800

    Make Log In view controller

--- LoginApp/View Controllers/WelcomeViewController.swift
+++ LoginApp/View Controllers/WelcomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WelcomeViewController.swift
 //  LoginApp
 //
 //  Created by Viktor Teslenko on 04.03.2023.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WelcomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
+    
 }
-
